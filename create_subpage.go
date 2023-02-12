0ce792f89cb813b7bd06aeeab736c67b75127fd1
shibe23 <shibe23.info@gmail.com>
2023-02-12 12:38:44 +0900

    test: add create subpage

---
+++ create_subpage.go
@@ -0,0 +1,105 @@
+package confluence_tool
+
+import (
+	"fmt"
+	"log"
+
+	goconfluence "github.com/virtomize/confluence-go-api"
+)
+
+type CreateSubpage struct{}
+
+func (c CreateSubpage) Parse(variable string) []string {
+	return []string{"鈴木", "佐藤", "田中", "堀井"}
+}
+
+// dummy
+func main() {
+	api, err := goconfluence.NewAPI("https://<your-domain>.atlassian.net/wiki/rest/api", "<username>", "<api-token>")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// get content by content id
+	c, err := api.GetContentByID("12345678", goconfluence.ContentQuery{
+		SpaceKey: "IM",
+		Expand:   []string{"body.storage", "version"},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%+v\n", c)
+
+	//get content by query
+	res, err := api.GetContent(goconfluence.ContentQuery{
+		SpaceKey: "IM",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, v := range res.Results {
+		fmt.Printf("%+v\n", v)
+	}
+
+	// create content
+	data := &goconfluence.Content{
+		Type:  "page",           // can also be blogpost
+		Title: "Some-Test-Page", // page title
+		Ancestors: []goconfluence.Ancestor{
+			goconfluence.Ancestor{
+				ID: "123456", // ancestor-id optional if you want to create sub-pages
+			},
+		},
+		Body: goconfluence.Body{
+			Storage: goconfluence.Storage{
+				Value:          "#api-test\nnew sub\npage", // your page content here
+				Representation: "storage",
+			},
+		},
+		Version: &goconfluence.Version{
+			Number: 1,
+		},
+		Space: &goconfluence.Space{
+			Key: "SomeSpaceKey", // Space
+		},
+	}
+
+	c, err = api.CreateContent(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%+v\n", c)
+
+	// update content
+	data = &goconfluence.Content{
+		ID:    "1234567",
+		Type:  "page",
+		Title: "updated-title",
+		Ancestors: []goconfluence.Ancestor{
+			{
+				ID: "2345678",
+			},
+		},
+		Body: goconfluence.Body{
+			Storage: goconfluence.Storage{
+				Value:          "#api-page\nnew\ncontent",
+				Representation: "storage",
+			},
+		},
+		Version: &goconfluence.Version{
+			Number: 2,
+		},
+		Space: &goconfluence.Space{
+			Key: "SomeSpaceKey",
+		},
+	}
+
+	c, err = api.UpdateContent(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%+v\n", c)
+
+}
---
+++ create_subpage_test.go
@@ -0,0 +1,28 @@
+package confluence_tool
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestParseValues(t *testing.T) {
+	c := CreateSubpage{}
+
+	type args struct {
+		variables string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{"改行区切りで受け取った名前をひとつずつ分ける", args{variables: "鈴木\n佐藤\n田中\n堀井"}, []string{"鈴木", "佐藤", "田中", "堀井"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := c.Parse(tt.args.variables); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Parse() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
